7bce90a689f3a68035399e4510e0a4793d5e5931
unknown <t109360141@ntut.org.tw>
2022-11-27 19:17:31 +0800

    first commit

---
+++ src/hw4/p14.java
@@ -0,0 +1,62 @@
+package hw4;
+
+public class p14 {
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new RacingCar();
+		
+		car1.setCar(1234, 20.5);
+		
+		car1.show();
+	}
+}
+
+class Car5
+{
+	protected int num;
+	protected double gas;
+	
+	public Car5() {
+		num = 0;
+		gas = 0.0;
+		System.out.println("生產了車子");
+	}
+	
+	public void setCar(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("將車號設為" + num + "，汽油量設為" + gas);
+	}
+	
+	public void show()
+	{
+		System.out.println("車號是" + num);
+		System.out.println("汽油量是" + gas);
+	}
+}
+
+class RacingCar5 extends Car5
+{
+	private int course;
+	
+	public RacingCar5()
+	{
+		course = 0;
+		System.out.println("生產了賽車");
+	}
+	
+	public void setCourse(int c)
+	{
+		course = c;
+		System.out.println("將賽車編號設為" + course);
+	}
+	
+	public void show()
+	{
+		System.out.println("賽車的車號是" + num);
+		System.out.println("汽油量是" + gas);
+		System.out.println("賽車編號是" + course);
+	}
+}
